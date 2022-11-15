5ab473a1ef861a24a7fea31d4c4a5de0f42204a8
catalyst-yuki-k <catalyst-yuki_k@hotmail.com>
2022-11-15 21:43:11 +0900

    追記

--- 01.ndarray/ufunc_Bit_twiddling_functions.py
+++ 01.ndarray/ufunc_Bit_twiddling_functions.py
@@ -65,25 +65,82 @@ print(np.binary_repr(28))
 print(np.bitwise_xor(31, 5))
 # 26
 print(np.bitwise_xor([31,3], 5))
-# array([26,  6])
+# [26  6]
 print(np.bitwise_xor([31,3], [5,6]))
-# array([26,  5])
+# [26  5]
 print(np.bitwise_xor([True, True], [False, True]))
-# array([ True, False])
+# [ True False]
 # The ^ operator can be used as a shorthand for np.bitwise_xor on ndarrays.
 x1 = np.array([True, True])
 x2 = np.array([False, True])
 print(x1 ^ x2)
-# array([ True, False])
+# [ True False]
 
 
 # invert(x, /[, out, where, casting, order, ...])
 # Compute bit-wise inversion, or bit-wise NOT, element-wise.
+# We’ve seen that 13 is represented by 00001101. The invert or bit-wise NOT of 13 is then:
+x = np.invert(np.array(13, dtype=np.uint8))
+print(x)
+# 242
+print(np.binary_repr(x, width=8))
+# 11110010
+#The result depends on the bit-width:
+x = np.invert(np.array(13, dtype=np.uint16))
+print(x)
+# 65522
+print(np.binary_repr(x, width=16))
+# 1111111111110010
+# When using signed integer types the result is the two’s complement of the result for the unsigned type:
+print(np.invert(np.array([13], dtype=np.int8)))
+# [-14]
+print(np.binary_repr(-14, width=8))
+# 11110010
+# Booleans are accepted as well:
+print(np.invert(np.array([True, False])))
+# [False  True]
+# The ~ operator can be used as a shorthand for np.invert on ndarrays.
+x1 = np.array([True, False])
+print(~x1)
+# [False  True]
 
 
 # left_shift(x1, x2, /[, out, where, casting, ...])
 # Shift the bits of an integer to the left.
+print(np.binary_repr(5))
+# 101
+print(np.left_shift(5, 2))
+# 20
+print(np.binary_repr(20))
+# 10100
+print(np.left_shift(5, [1,2,3]))
+# [10 20 40]
+# Note that the dtype of the second argument may change the dtype of the result and can lead to unexpected results in some cases (see Casting Rules):
+a = np.left_shift(np.uint8(255), 1) # Expect 254
+print(a, type(a)) # Unexpected result due to upcasting
+# 510 <class 'numpy.intc'>
+b = np.left_shift(np.uint8(255), np.uint8(1))
+print(b, type(b))
+# 254 <class 'numpy.uint8'>
+# The << operator can be used as a shorthand for np.left_shift on ndarrays.
+x1 = 5
+x2 = np.array([1, 2, 3])
+print(x1 << x2)
+# [10 20 40]
 
 
 # right_shift(x1, x2, /[, out, where, ...])
 # Shift the bits of an integer to the right.
+print(np.binary_repr(10))
+# 1010
+print(np.right_shift(10, 1))
+# 5
+print(np.binary_repr(5))
+# 101
+print(np.right_shift(10, [1,2,3]))
+# [5 2 1]
+# The >> operator can be used as a shorthand for np.right_shift on ndarrays.
+x1 = 10
+x2 = np.array([1,2,3])
+print(x1 >> x2)
+# [5 2 1]
